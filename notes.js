67d5c7750304c4c2e1613bdb6460a1a8b4ff9df8
Anthony Simental <anthonysimental12@gmail.com>
2022-12-08 21:40:09 -0800

    commit tree 76438b085b0fb0b4104d2ad51930ce2a7b09f8bc

--- notes.js
+++ notes.js
@@ -1,12 +1,14 @@
+// Arrow Functions
 // function divide (a, b) {
 //     return a / b;
 // }
 // console.log(divide(2000, 100)); // => 20
 
 const divide = (a, b) => a / b;
-
+console.log(divide(2000, 100));
 
 const square = a => a * a;
-
+console.log(square(4)); // => 16
 
 const add = (a, b) => a + b;
+console.log(add(12, 20)); // => 32
